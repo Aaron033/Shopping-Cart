5e5ea8558bae37e58190548ec1e64d9b05ce674d
Aaron Botello <aaron.botello.h@gmail.com>
2019-07-23 12:03:51 -0500

    adding content

---
+++ app.js
@@ -0,0 +1,33 @@
+function calculator(x , y){
+    return { 
+
+        addition() { 
+            console.log(x + y)
+        }
+    }
+}
+
+
+
+
+
+
+// function add(x, y){ 
+
+//     return { 
+
+//         addition() { 
+
+//             console.log(x + y)
+
+//         }
+
+//     }
+
+// }
+
+// const suma = add(23,45)
+
+// console.log(suma)
+
+
