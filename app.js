e468e7f8843de316ed9a90fe23ae92aa578ec94b
Aaron Botello <aaron.botello.h@gmail.com>
2019-08-05 10:38:10 -0500

    calc added

--- app.js
+++ app.js
@@ -1,33 +1,5 @@
-function calculator(x , y){
-    return { 
-
-        addition() { 
-            console.log(x + y)
-        }
-    }
-}
 
 
-
-
-
-
-// function add(x, y){ 
-
-//     return { 
-
-//         addition() { 
-
-//             console.log(x + y)
-
-//         }
-
-//     }
-
-// }
-
-// const suma = add(23,45)
-
-// console.log(suma)
-
-
+ let zero = document.getElementById("ze").addEventListener("click", function(){
+     console.log("yeii")
+ })
