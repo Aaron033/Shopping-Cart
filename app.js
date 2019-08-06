77985a84cd3f1b8eef2118c6124d44b6a7022375
Aaron Botello <aaron.botello.h@gmail.com>
2019-08-06 11:53:17 -0500

    footer created

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ class Automobile {
 
 let dodge = new Automobile("dodge", "ram", "2012", "14000");
 let chevy = new Automobile("chevy", "Silverado", "2018", "24000");
-let d = dodge.displayAuto();
+
 chevy.displayAuto();
 
-let auto1 = document.querySelector('.auto1').innerHTML= d;
+let auto1 = document.querySelector('.auto1').innerHTML= this.dodge;
